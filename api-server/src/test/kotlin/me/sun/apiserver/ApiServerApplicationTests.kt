c979a65e1c40d6f179999272fbb2a977fcc047ce
dongmyeonglee <dongmyeong.lee22@gmail.com>
2021-06-22 23:50:09 +0900

    init project

---
+++ api-server/src/test/kotlin/me/sun/apiserver/ApiServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.sun.apiserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
